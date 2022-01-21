64b5b470dde6ea6b680a4bbabd25d8008897b91b
Mohammed Mohsin <60876832+mohammed-mohsin@users.noreply.github.com>
2022-01-21 11:49:33 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//     window.addEventListener('scroll', function() {
+//         if (window.scrollY > 750) {
+//           document.getElementById('nav1').classList.add('fixed-top');
+//           document.getElementById('nav1').classList.add('bg-dark');
+//           // add padding top to show content behind navbar
+//         //   navbar_height = document.querySelector('.navbar').offsetHeight;
+//         //   document.body.style.paddingTop = navbar_height + 'px';
+//         } else if(window.scrollY < 750){
+//           document.getElementById('navbar_top').classList.remove('fixed-top');
+//            // remove padding top from body
+//         //   document.body.style.paddingTop = '0';
+//         } 
+//     });
+//   }); 
+
+  
