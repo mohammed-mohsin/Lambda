c46638cd185e86e32562da11337461213f5411a8
Mohammed Mohsin <60876832+mohammed-mohsin@users.noreply.github.com>
2022-01-21 13:56:49 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 // document.addEventListener("DOMContentLoaded", function(){
 //     window.addEventListener('scroll', function() {
-//         if (window.scrollY > 750) {
-//           document.getElementById('nav1').classList.add('fixed-top');
-//           document.getElementById('nav1').classList.add('bg-dark');
+//         if (window.scrollY > 1150) {
+//           document.getElementById('scrollToTopBtn').classList.add('d-block');
+
 //           // add padding top to show content behind navbar
 //         //   navbar_height = document.querySelector('.navbar').offsetHeight;
 //         //   document.body.style.paddingTop = navbar_height + 'px';
-//         } else if(window.scrollY < 750){
-//           document.getElementById('navbar_top').classList.remove('fixed-top');
+//         } else {
+//           document.getElementById('navbar_top').classList.remove('d-none');
 //            // remove padding top from body
 //         //   document.body.style.paddingTop = '0';
 //         } 
 //     });
 //   }); 
 
-  
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()>750)
+     {
+        $('#scrollToTopBtn').fadeIn();
+     }
+    else
+     {
+      $('#scrollToTopBtn').fadeOut();
+     }
+ });
